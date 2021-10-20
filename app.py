10b7fae88847993fc14da4e573d59d9907fe58eb
OleksiiMalyshev <86618537+OleksiiMalyshev@users.noreply.github.com>
2021-10-20 21:52:22 +0300

    Completed app with template file answer.html

--- app.py
+++ app.py
@@ -1,26 +1,40 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=["GET","POST"])
+@app.route('/', methods=["GET", "POST"])
 def index():
     return 'Hello world'
 
+
 @app.route('/<a>/<b>/div')
 def division(a, b):
-    return f"{a} / {b} = {int(a)//int(b)}"
+    sign = '/'
+    answer = int(a) // int(b)
+    return render_template('answer.html', a=a, b=b, sign=sign, answer=answer)
+
 
 @app.route('/<a>/<b>/mult')
 def multiply(a, b):
-    return f"{a} * {b} = {int(a)*int(b)}"
+    sign = '*'
+    answer = int(a) * int(b)
+    return render_template('answer.html', a=a, b=b, sign=sign, answer=answer)
+
 
 @app.route('/<a>/<b>/plus')
 def plus(a, b):
-    return f"{a} + {b} = {int(a)+int(b)}"
+    sign = '+'
+    answer = int(a) + int(b)
+    return render_template('answer.html', a=a, b=b, sign=sign, answer=answer)
+
 
 @app.route('/<a>/<b>/minus')
 def minus(a, b):
-    return f"{a} - {b} = {int(a)-int(b)}"
+    sign = '-'
+    answer = int(a) - int(b)
+    return render_template('answer.html', a=a, b=b, sign=sign, answer=answer)
+
 
 app.run(host='0.0.0.0', port=8080, debug=True)
