39f80ce7fe3e435abbb6f73145ae96d52da29e86
OleksiiMalyshev <86618537+OleksiiMalyshev@users.noreply.github.com>
2021-10-30 19:55:03 +0300

    Homework #15 first commit on flask_hw_3 branch

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
 class Config:
     WEATHER_API_KEY=os.getenv("WEATHER_API_KEY")
     WEATHER_API_URL=os.getenv("WEATHER_API_URL")
--- routes/weather.py
+++ routes/weather.py
@@ -1,8 +1,7 @@
-from flask import request, render_template, jsonify
+from flask import request, render_template
 from app import app
 from config import Config
 import requests
-
 
 @app.route('/weather')
 def weather():
@@ -10,19 +9,9 @@ def weather():
     response = requests.get(
         Config.WEATHER_API_URL + "?key=" + Config.WEATHER_API_KEY + "&q=" + city + "&aqi=yes"
     )
+
     return response.json()
 
-
-@app.route('/weather-for-cities')
-def weather_for_cities():
-    big_response = {}
-    cities = request.args.get('cities')
-    for city in cities.split(' '):
-        response = requests.get(
-            Config.WEATHER_API_URL + "?key=" + Config.WEATHER_API_KEY + "&q=" + city + "&aqi=yes"
-        )
-        big_response[city] = response.json()
-    return jsonify(big_response)
 
 
 @app.route('/get-your-weather')
