926259093a8e44673e6ac481571f7c6405def87f
OleksiiMalyshev <86618537+OleksiiMalyshev@users.noreply.github.com>
2021-11-01 02:04:17 +0200

    Homework #15 delete all list items function

--- app.py
+++ app.py
@@ -5,9 +5,6 @@ from config import Config
 app = Flask(__name__)
 api = Api(app)
 
-print(Config.WEATHER_API_KEY)
-print(Config.WEATHER_API_URL)
-
 with app.app_context():
     from routes.lecture1 import *
     from routes.weather import *
--- routes/api/todo.py
+++ routes/api/todo.py
@@ -3,13 +3,20 @@ from flask import request
 from flask_restful import Resource
 
 todos = []
+post_id = 0
 
 class Todo(Resource):
     def get(self):
-        return todos
+        return todos, post_id
 
     def post(self):
         todos.append(request.json)
-        return todos
+        return todos, post_id
+
+    def delete(self):
+        todos.clear()
+        return todos, post_id
+
+
 
 api.add_resource(Todo, "/api/v1/todos")
