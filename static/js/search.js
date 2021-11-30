f5bfe079b21c466a3e2a24a77360ec68cec20b32
OleksiiMalyshev <86618537+OleksiiMalyshev@users.noreply.github.com>
2021-11-30 22:02:05 +0200

    hw# 24 completed

--- static/js/search.js
+++ static/js/search.js
@@ -1,6 +1,24 @@
 $(document).ready(function () {
     $("#searchInput").on('keyup', function (event) {
         let text = $(this).val();
-        console.log(text);
-    })
+        if (text != ''){
+            $.ajax({
+            url: "/api/v1/search?q="+text,
+            type: "GET",
+            contentType: "application/json; charset=utf-8",
+            success: (elems) =>{
+                $.each (elems, function (index,val) {
+                    $.each(val, function(key,value){
+                    if (key === 'name'){
+                            $("#searchResults").append("<li>"+ value + "</li>");
+                    }
+                    });
+                    });
+                }
+            });
+        }
+        else{
+            $("#searchResults").empty();
+        }
+    });
 });
